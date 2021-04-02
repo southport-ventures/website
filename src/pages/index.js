4cd8fcd44716a8b30035a9a6135831a0d9bb2e1c
Nicholas Evans <nicholasaevans@gmail.com>
2021-04-01 23:24:17 -0700

    Add contrasting element

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import SketchingTogether from "../images/sketching-together.jpg"
+import ContrastElement from "../images/contrast-element.png"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Southport Ventures" />
     <section className="bg-blue-200 relative">
       <div className="lg:container mx-auto flex flex-wrap md:flex-nowrap xl:items-center">
-        <div className="w-100 md:w-75 px-5 py-20 md:px-20 lg:pr-40 md:py-20 xl:py-32">
+        <div className="w-100 md:w-75 px-5 py-20 md:px-20 md:pt-20 md:pb-0 xl:pt-32 xl:pb-11">
           <h1 className="text-4xl lg:text-5xl leading-tight sm:leading-tight md:leading-tight lg:leading-tight">
             We buy and grow software businesses
           </h1>
@@ -23,12 +24,13 @@ const IndexPage = () => (
           </Link>
         </div>
       </div>
+      <img className="hidden md:block w-full h-24 object-none object-right-bottom" src={ContrastElement} alt="Contrast graphic" />
     </section>
 
     <section className="lg:container mx-auto md:mt-20 lg:my-40 md:px-20">
-      <div className="md:flex md:items-end relative">
-        <img className="w-full md:w-9/12" src={SketchingTogether} alt="Two people looking at computer together" />
-        <div className="bg-blue-700 px-5 py-20 md:p-14 z-10 md:absolute md:right-0 xl:right-10 md:bottom-0 md:w-6/12 xl:w-4/12">
+      <div className="lg:flex lg:items-end relative">
+        <img className="w-full lg:w-9/12" src={SketchingTogether} alt="Two people looking at computer together" />
+        <div className="bg-blue-700 px-5 py-20 md:p-14 z-10 lg:absolute lg:right-0 xl:right-10 lg:bottom-0 lg:w-6/12 xl:w-4/12">
           <h2 className="text-3xl lg:text-2xl text-blue-200">
             How We're Different
           </h2>
@@ -62,7 +64,7 @@ const IndexPage = () => (
         <div className="bg-white p-10 shadow-xl mb-10">
           <h4 className="text-xl mb-2">Revenue & Profitability</h4>
           <p>
-            Profitable businesses with $1M to $7M in annual recurring revenue.
+            Profitable businesses with $500K to $5M in annual recurring revenue.
           </p>
         </div>
         <div className="bg-white p-10 shadow-xl mb-10">
